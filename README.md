1b6a52ce074fa5e8cf179c05756c5719f1f7fcb9
Christopher Goes <cwgoes@pluranimity.org>
2019-03-15 23:16:09 +0100

    Working...

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 ### StakePi
 
 Monitoring staking status with a Raspberry Pi
+
+#### Hardware
+
+Can easily be varied.
+
+- [Raspberry Pi 3 B+]()
+- [Qwiic Hat]()
+- [BME280]()
+- [Qwiic LED Stick]()
+- [Micro OLED]()
+
+#### Setup
+
+```
+pip2 install -r requirements.txt
+```
+
+And install [this fork](https://github.com/cwgoes/Adafruit_Python_SSD1306) of the SSD1306 library.
+
+#### Usage
+
+```bash
+./monitor.py
+```
--- monitor.py
+++ monitor.py
@@ -4,8 +4,25 @@ import math
 import requests
 import yaml
 import util
+import time
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
-config = yaml.load(open('config.yaml'), Loader = yaml.CLoader)
+config = yaml.load(open('config.yaml'))
+
+disp = Adafruit_SSD1306.SSD1306_64_48(rst = None, i2c_address = 0x3d)
+disp.clear()
+disp.display()
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+draw = ImageDraw.Draw(image)
+draw.rectangle((0, 0, width, height), outline = 0, fill = 0)
+
+font = ImageFont.load_default()
 
 def autoformat(value):
     if value > 1e6:
@@ -13,6 +30,9 @@ def autoformat(value):
     if value > 1e3:
         return str(round(value / 1e3, 2)) + 'k'
     return round(value, 2)
+
+def fetch_temperature():
+    return util.readBME280All()[0]
 
 def fetch_cmc_price(ticker):
     return float(requests.get('https://api.coinmarketcap.com/v1/ticker/' + ticker + '/').json()[0]['price_usd'])
@@ -29,6 +49,12 @@ def get_cosmos_stake():
     staking_balance = float(requests.get('https://sgapi.certus.one/validator/{}'.format(config['addresses']['cosmos'])).json()['app_data']['tokens']) / 1e6
     return staking_balance
 
+def display(text):
+    draw.rectangle((0, 0, width, height), outline = 0, fill = 0)
+    draw.text((8, -2), str(text), font = font, fill = 255)
+    disp.image(image)
+    disp.display()
+
 def update_loop():
     stakes = [
         ('tezos', get_tezos_stake()),
@@ -44,4 +70,5 @@ def update_loop():
     return (stakes, prices, total_staked)
 
 print(update_loop())
-util.main()
+print('Temp: {}'.format(fetch_temperature()))
+display('testing')
